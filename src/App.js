1e574faeb4cfc256144a2cbaa8a6cdaaf223bc1d
Pedro Bufulin <pedro.almeida@ufu.br>
2020-01-24 02:41:26 -0300

    adicionadas as classes de css. Tem um problema na responsividade

--- src/App.js
+++ src/App.js
@@ -1,13 +1,90 @@
-import React, { Component } from "react";
-import Header from "./components/Header";
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header title="titulo 1 "></Header>
-      </React.Fragment>
-    );
-  }
+import React, { useState } from "react";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+function App() {
+  return (
+    <div id="App">
+      <aside>
+        <strong className="">Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username"> Usuário do github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs"> Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor=""> Latitude </label>
+              <input name="lat" id="lat" required></input>
+            </div>
+            <div className="input-block">
+              <label htmlFor=""> Latitude </label>
+              <input name="lon" id="lon" required></input>
+            </div>
+          </div>
+          <button type="submit"> Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
+
+              <div className="user-info">
+                <strong>Pedro Bufulin</strong>
+                <span>React,lips,C</span>
+              </div>
+            </header>
+            <p>dev em uberlnadia</p>
+            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
+
+              <div className="user-info">
+                <strong>Pedro Bufulin</strong>
+                <span>React,lips,C</span>
+              </div>
+            </header>
+            <p>dev em uberlnadia</p>
+            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
+
+              <div className="user-info">
+                <strong>Pedro Bufulin</strong>
+                <span>React,lips,C</span>
+              </div>
+            </header>
+            <p>dev em uberlnadia</p>
+            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
+
+              <div className="user-info">
+                <strong>Pedro Bufulin</strong>
+                <span>React,lips,C</span>
+              </div>
+            </header>
+            <p>dev em uberlnadia</p>
+            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
