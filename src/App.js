4a6fc1a05072c71584235bf57c0d6d761d689f38
Pedro Bufulin <pedro.almeida@ufu.br>
2020-01-24 20:39:26 -0300

    finalizadas a aula da interface web. Falta colocar o exercício extra de edição e deleção

--- src/App.js
+++ src/App.js
@@ -3,99 +3,40 @@ import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
+import api from "./services/api";
+import DevItem from "./DevItem/index";
+import DevForm from "./DevForm/index";
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        console.log(position);
-      },
-      err => {
-        console.log(err);
-      },
-      {
-        timeout: 30000
-      }
-    );
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
   }, []);
+
+  async function handleAddDev(data) {
+    console.log(data);
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+    console.log(response.data);
+  }
 
   return (
     <div id="App">
       <aside>
-        <strong className="">Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username"> Usuário do github</label>
-            <input name="github_username" id="github_username" required></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs"> Tecnologias</label>
-            <input name="techs" id="techs" required></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor=""> Latitude </label>
-              <input name="lat" id="lat" required></input>
-            </div>
-            <div className="input-block">
-              <label htmlFor=""> Latitude </label>
-              <input name="lon" id="lon" required></input>
-            </div>
-          </div>
-          <button type="submit"> Salvar</button>
-        </form>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}></DevForm>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
-
-              <div className="user-info">
-                <strong>Pedro Bufulin</strong>
-                <span>React,lips,C</span>
-              </div>
-            </header>
-            <p>dev em uberlnadia</p>
-            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
-
-              <div className="user-info">
-                <strong>Pedro Bufulin</strong>
-                <span>React,lips,C</span>
-              </div>
-            </header>
-            <p>dev em uberlnadia</p>
-            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
-
-              <div className="user-info">
-                <strong>Pedro Bufulin</strong>
-                <span>React,lips,C</span>
-              </div>
-            </header>
-            <p>dev em uberlnadia</p>
-            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/39497156?s=460&v=4"></img>
-
-              <div className="user-info">
-                <strong>Pedro Bufulin</strong>
-                <span>React,lips,C</span>
-              </div>
-            </header>
-            <p>dev em uberlnadia</p>
-            <a href="https://github.com/pedrohba1">Acessar perfil no github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}></DevItem>
+          ))}
         </ul>
       </main>
     </div>
